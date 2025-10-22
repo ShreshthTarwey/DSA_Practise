d6d2d569335eb92f0b7c4a6d5c3dd423244cb963
Shreshth Tarwey <151712070+ShreshthTarwey@users.noreply.github.com>
2025-10-22 22:16:02 +0530

    Time: 18 ms (15.99%) | Memory: 60.8 MB (5.17%) - LeetSync

--- 415-add-strings/add-strings.cpp
+++ 415-add-strings/add-strings.cpp
@@ -43,8 +43,31 @@ public:
         }
         return res;
     }
+    string recursiveMethod(string &num1, int i,string &num2, int j, int carry=0){
+        //base case
+        if(i<0 && j<0){
+            if(carry!=0){
+                return string(1,carry+'0');
+            }
+            return "";
+        }
+
+        string res = "";
+        int n1 = (i>=0 ? num1[i] : '0') - '0';
+        int n2 = (j>=0 ? num2[j] : '0') - '0';
+        int sum = n1+n2+carry;
+        int digit = sum%10;
+        carry = sum/10;
+        res.push_back(digit+'0');
+
+        res += recursiveMethod(num1, i-1, num2, j-1, carry);
+        return res;
+    }
     string addStrings(string num1, string num2) {
         // Method 1 ---> using loops
-        return usingLoops(num1, num2);
+        // return usingLoops(num1, num2); //T.C -> O(Max(N,M))
+        string res = recursiveMethod(num1,num1.length()-1,num2,num2.length()-1);
+        reverse(res.begin(),res.end());
+        return res;
     }
 };
