10b57e983f9f9a0a6ba161c494846d6bb3e668c4
Shreshth Tarwey <151712070+ShreshthTarwey@users.noreply.github.com>
2025-10-22 21:45:55 +0530

    Time: 42 ms (6.73%) | Memory: 58.9 MB (10.43%) - LeetSync

---
+++ 415-add-strings/add-strings.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    string usingLoops(string& num1, string& num2) {
+        int carry = 0;
+        // int i = min(num1.length(),num2.length());
+        string res = "";
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+        while (i >= 0 && j >= 0) {
+            int sum = (num1[i]-'0') + (num2[j]-'0') + carry;
+            carry = sum / 10;
+            res = (char)((sum % 10)+'0') + res;
+            // cout<<res<<endl;
+            i--;
+            j--;
+        }
+        // cout<<carry<<endl;
+        cout<<res<<endl;
+        cout<<"This is i"<<i<<endl;
+        while(i>=0){
+            int sum = carry + (num1[i]-'0');
+            cout<<(num1[i]-'0')<<endl;
+            // if(sum>=10)
+            res = (char)((sum%10)+'0') + res;
+            // else if(sum!=0)
+            // res = (char)(sum+'0') + res;
+            cout<<res<<endl;
+            carry = sum/10;
+            i--;
+        }
+        cout<<res<<endl;
+        while(j>=0){
+            cout<<"Enter in J?"<<endl;
+            int sum = carry + (num2[j]-'0');
+            // BUG 1 FIX: Swap logic
+            carry = sum / 10;                     // This is the new carry
+            res = (char)((sum % 10) + '0') + res; // This is the new digit
+            j--;
+        }
+        cout<<"last carry"<<carry<<endl;
+        if (carry > 0) {
+            res = (char)(carry + '0') + res;
+        }
+        return res;
+    }
+    string addStrings(string num1, string num2) {
+        // Method 1 ---> using loops
+        return usingLoops(num1, num2);
+    }
+};
