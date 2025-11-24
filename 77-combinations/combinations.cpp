94e79c1db414e318b64c3e5b165573973896ad9f
Shreshth Tarwey <151712070+ShreshthTarwey@users.noreply.github.com>
2025-11-24 21:53:25 +0530

    Time: 41 ms (52.33%) | Memory: 62.7 MB (52.68%) - LeetSync

---
+++ 77-combinations/combinations.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void backtracking_method(vector<vector<int>>&res,vector<int>&temp,vector<int>&nums,int start_index,int k){
+        if(temp.size()==k){
+            res.push_back(temp);
+        }
+        for(int i=start_index;i<nums.size();i++){
+            temp.push_back(nums[i]);
+            backtracking_method(res,temp,nums,i+1,k);
+            temp.pop_back();
+        }
+    }
+    vector<vector<int>> combine(int n, int k) {
+        vector<int>temp;
+        vector<vector<int>>res;
+        vector<int>nums;
+        for(int i=1;i<=n;i++){
+            nums.push_back(i);
+        }
+        backtracking_method(res,temp,nums,0,k);
+        return res;
+    }
+};
