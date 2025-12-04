e7ce0ba21c914e1fa6c7118b21eb0ef5428eeeaf
Shreshth Tarwey <151712070+ShreshthTarwey@users.noreply.github.com>
2025-12-04 15:45:19 +0530

    Time: 3 ms (7.69%) | Memory: 9.8 MB (12.21%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -11,7 +11,22 @@ public:
         ans.push_back(temp);
         return ans;
     }
+    vector<vector<int>> sirMethod(int n){
+        vector<vector<int>> ans;
+        
+        for(int i=0; i<n;i++){
+            vector<int> row;
+            for(int j=0; j<=i;j++){
+                if(j==0 || j==i) row.push_back(1);
+                else row.push_back(ans[i-1][j-1] + ans[i-1][j]);
+            }
+            ans.push_back(row);
+            row.clear();
+        }
+        return ans;
+    }
     vector<vector<int>> generate(int numRows) {
-        return method1(numRows);
+        // return method1(numRows);
+        return sirMethod(numRows);
     }
 };
