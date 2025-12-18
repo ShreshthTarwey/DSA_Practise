08f12b5834af30f2777c106aed15dff29f36b874
Shreshth Tarwey <151712070+ShreshthTarwey@users.noreply.github.com>
2025-12-18 13:49:51 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (41.29%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -19,7 +19,37 @@ public:
     }
     int minDistance(string word1, string word2) {
         //Memoization
-        vector<vector<int>>dp(word1.length(), vector<int>(word2.length(), -1));
-        return recFun(word1, word2, 0, 0, dp);
+        // vector<vector<int>>dp(word1.length(), vector<int>(word2.length(), -1));
+        // return recFun(word1, word2, 0, 0, dp);
+
+        //----------------------------------TABULATION-------------------------------------
+        vector<vector<int>>dp(word1.length()+1, vector<int>(word2.length()+1, 0));
+        int n = word1.length();
+        int m = word2.length();
+
+        for(int j=0;j<m;j++){
+            dp[n][j] = m-j;
+        }
+        for(int i=0;i<n;i++){
+            dp[i][m] = n-i;
+        }
+
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                if(word1[i] == word2[j]){
+                    dp[i][j] = dp[i+1][j+1];
+                    continue;
+                }
+
+                int insert = 1 + dp[i][j+1];
+
+                int del = 1 + dp[i+1][j];
+
+                int replace = 1 + dp[i+1][j+1];
+
+                dp[i][j] = min(insert, min(del, replace));
+            }
+        }
+        return dp[0][0];
     }
 };
