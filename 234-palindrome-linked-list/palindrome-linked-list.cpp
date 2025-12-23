f7f4f02c933396c1594f2c382c0aca5444972417
Shreshth Tarwey <151712070+ShreshthTarwey@users.noreply.github.com>
2025-12-23 21:47:38 +0530

    Time: 5 ms (34.68%) | Memory: 118.1 MB (56.80%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -16,10 +16,53 @@ public:
         recursion(head->next, forward, backward);
         backward.push_back(head->val);
     }
+
+    ListNode* reverseNode(ListNode* head){
+        if(!head) return NULL;
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* fut = head->next;
+        while(curr){
+            curr->next = prev;
+            prev = curr;
+            curr = fut;
+            if(fut!=NULL)
+            fut = fut->next;
+        }
+        return prev;
+    }
+    bool sirMethod(ListNode* head){
+        //First finding the middle pointer, or node
+        ListNode* slow = head;
+        ListNode* fast = head;
+        ListNode* prev = slow;
+        while(fast && fast->next){
+            prev = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        prev->next = NULL;
+        ListNode* head2 = reverseNode(slow);
+        ListNode* pointer = head;
+        while(pointer){
+            if(pointer->val != head2->val) return false;
+            pointer = pointer->next;
+            head2 = head2->next;
+        }
+
+        return true;
+        
+    }
+
+
     bool isPalindrome(ListNode* head) {
-        vector<int>forward;
-        vector<int>backward;
-        recursion(head, forward, backward);
-        return forward == backward;
+        //My Method --------------------------------------------------------------------------------------------
+        // vector<int>forward;
+        // vector<int>backward;
+        // recursion(head, forward, backward);
+        // return forward == backward;
+        // --------------------------------------------------------------------------------------------------------
+        // SirMethod, one of the application of 2 pointer
+        return sirMethod(head);
     }
 };
