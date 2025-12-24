685ed32391da8a428284e6226b9cf5c47ebe82da
Shreshth Tarwey <151712070+ShreshthTarwey@users.noreply.github.com>
2025-12-24 23:12:37 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (95.39%) - LeetSync

---
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int getLength(ListNode* head){
+        ListNode* temp = head;
+        int count = 0;
+        while(temp){
+            temp = temp->next;
+            count++;
+        }
+        return count;
+    }
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        //Base Cases
+        if(!head) return NULL;
+        if(!head->next) return head;
+
+        //1 case hum krange baki recursion sambhal lega
+
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* fut = head->next;
+
+        int position = 0;
+
+        int len = getLength(head);
+        if(len<k){
+            return head;
+        }
+        while(position<k){
+            curr->next = prev;
+            prev = curr;
+            curr = fut;
+            if(fut!=NULL) fut = fut->next;
+            position++;
+        }
+        ListNode* recursionKaHead = reverseKGroup(curr, k);
+        head->next = recursionKaHead;
+        return prev;
+    }
+};
