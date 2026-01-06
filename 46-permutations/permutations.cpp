28332c02a7fb9a52c54fbc60295989755f364754
Shreshth Tarwey <151712070+ShreshthTarwey@users.noreply.github.com>
2026-01-06 13:37:56 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (87.27%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,47 +1,19 @@
 class Solution {
 public:
-    void solve(vector<int>& nums, int i, vector<vector<int>>& ans){
-        //Base Case
-        if(i>=nums.size()){
+    void solve(vector<vector<int>>& ans, int idx, vector<int>& nums){
+        if(idx == nums.size()){
             ans.push_back(nums);
             return;
         }
-
-        //1 Case solving from recursion
-
-        for(int j=i;j<nums.size();j++){
-            swap(nums[i],nums[j]);
-            solve(nums, i+1, ans);
-            //Backtracking
-            swap(nums[i],nums[j]);
+        for(int i=idx;i<nums.size();i++){
+            swap(nums[i], nums[idx]);
+            solve(ans, idx+1, nums);
+            swap(nums[i], nums[idx]);
         }
-    }
-    unordered_set<int>st;
-    void solve_method2(vector<int>& nums, vector<int> &temp, vector<vector<int>>& ans){
-        if(temp.size() == nums.size()){
-            ans.push_back(temp);
-            return;
-        }
-        
-
-        for(int i=0;i<nums.size();i++){
-            if(st.find(nums[i])==st.end()){
-                st.insert(nums[i]);
-                temp.push_back(nums[i]);
-                solve_method2(nums, temp, ans);
-                temp.pop_back();
-                st.erase(nums[i]);
-            }
-        }
-
     }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>>ans;
-        int i = 0;
-        // solve(nums,i,ans);
-        vector<int>temp;
-        solve_method2(nums, temp, ans);
+        solve(ans, 0, nums);
         return ans;
     }
-
 };
