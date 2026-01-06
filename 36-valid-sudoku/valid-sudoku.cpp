e940ebcf0b21318c563495785eeb772a84ae5ff0
Shreshth Tarwey <151712070+ShreshthTarwey@users.noreply.github.com>
2026-01-06 21:33:27 +0530

    Time: 0 ms (100.00%) | Memory: 21.6 MB (93.06%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
-    bool checkFun(vector<vector<char>>& board, int i, int j, char target){
-        //checking right
-        for(int k=j+1;k<board.size();k++){
-            if(target==board[i][k]){
+    bool check(vector<vector<char>>& board, int row, int col, char target){
+        //Right side check
+        for(int j = col+1;j<board.size();j++){
+            if(board[row][j]==target){
                 return false;
             }
         }
 
-        //checking down
-
-        for(int k=i+1;k<board.size();k++){
-            if(target==board[k][j]) return false;
+        //down side check
+        for(int j=row+1;j<board.size();j++){
+            if(board[j][col]==target){
+                return false;
+            }
         }
 
-        //Finding 3x3
+        //3x3 check
         for(int k=0;k<board.size();k++){
-            int row = 3*(i/3) + k/3;
-            int col = 3*(j/3) + k%3;
-            if (row == i && col == j) continue;
-            if(board[row][col] == target) return false;
+            int row1 = 3*(row/3) + k/3;
+            int col1 = 3*(col/3) + k%3;
+            if(row1 == row || col1 == col){
+                continue;
+            }
+            if(board[row1][col1] == target) return false;
+
         }
 
         return true;
@@ -27,10 +31,12 @@ public:
     }
     bool isValidSudoku(vector<vector<char>>& board) {
         for(int i=0;i<board.size();i++){
-            for(int j=0;j<board.size();j++){
-                if (board[i][j] == '.') continue;
-                bool check = checkFun(board, i, j, board[i][j]);
-                if(!check) return false;
+            for(int j=0;j<board[0].size();j++){
+                if(board[i][j] == '.') continue;
+                if(check(board, i,j,board[i][j])){
+                    continue;
+                }
+                return false;
             }
         }
         return true;
