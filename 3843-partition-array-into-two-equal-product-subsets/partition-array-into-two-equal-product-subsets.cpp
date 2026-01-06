9983082b28603d6c78963880b2aaeb97af51598c
Shreshth Tarwey <151712070+ShreshthTarwey@users.noreply.github.com>
2026-01-06 11:56:17 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (50.86%) - LeetSync

--- 3843-partition-array-into-two-equal-product-subsets/partition-array-into-two-equal-product-subsets.cpp
+++ 3843-partition-array-into-two-equal-product-subsets/partition-array-into-two-equal-product-subsets.cpp
@@ -1,36 +1,19 @@
 class Solution {
 public:
-    bool solve(vector<int>& nums, long long target, __int128 product, int i, __int128 currP, bool found){
-        if(i>=nums.size()){
-            if (i >= nums.size()) {
-            // Return true only if we actually found subset with product == target
-            return found && (target == (product / target));
-        }
-        }
-        if(currP==target){
-            if(target==(product/target)){
-                found = true;
-                return true;
+    bool recFun(vector<int>& nums, int index, long long prod, long long target){
+        if(index>=nums.size() || prod>target) return false;
+        if(prod == target) return true;
+        bool pick = recFun(nums, index+1, prod*nums[index], target);
+        bool notPick = recFun(nums, index+1, prod, target);
 
-            }
-            return false;
-        }
-        if(currP>target){
-            return false;
-        }
-
-        bool take = solve(nums, target, product, i+1, currP*nums[i],found);
-        bool notTake = solve(nums, target, product, i+1, currP,found);
-
-        return (take||notTake);
+        return (pick || notPick);
     }
     bool checkEqualPartitions(vector<int>& nums, long long target) {
         __int128 product = 1;
-        for(int i=0;i<nums.size();i++){
-            product*=nums[i];
+        for(auto it:nums){
+            product*=it;
         }
-        if (product > (__int128)target * target) return false;
-
-        return solve(nums, target, product, 0,1,false);
+        if(product!=(__int128)target*(__int128)target) return false;
+        return recFun(nums, 0, 1, target);
     }
 };
