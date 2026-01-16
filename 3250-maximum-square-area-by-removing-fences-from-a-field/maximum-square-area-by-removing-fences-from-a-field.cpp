3776eb342346f18e18864f898541b82231063f00
Shreshth Tarwey <151712070+ShreshthTarwey@users.noreply.github.com>
2026-01-16 10:41:40 +0530

    Time: 1738 ms (33.98%) | Memory: 445.7 MB (19.42%) - LeetSync

--- 3250-maximum-square-area-by-removing-fences-from-a-field/maximum-square-area-by-removing-fences-from-a-field.cpp
+++ 3250-maximum-square-area-by-removing-fences-from-a-field/maximum-square-area-by-removing-fences-from-a-field.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
-    void fillMap(vector<int>& arr, unordered_map<int, int>& mp){
+    void fillMap(vector<int>& arr, unordered_set<int>& mp){
+        sort(arr.begin(), arr.end());
         for(int i=0;i<arr.size();i++){
-            for(int j=0;j<arr.size();j++){
-                if(i==j) continue;
-                mp[abs(arr[i]-arr[j])]++;
+            for(int j=i+1;j<arr.size();j++){
+                // if(i==j) continue;
+                mp.insert(arr[j]-arr[i]);
             }
         }
     }
     int maximizeSquareArea(int m, int n, vector<int>& hFences, vector<int>& vFences) {
-        unordered_map<int, int>lengths;
-        unordered_map<int, int>widths;
+        unordered_set<int>lengths;
+        unordered_set<int>widths;
         hFences.push_back(1);
         hFences.push_back(m);
         vFences.push_back(1);
@@ -19,8 +20,8 @@ public:
         fillMap(vFences, widths);
         long long maxAns = -1;
         for(auto it: lengths){
-            if(widths.find(it.first)!=widths.end()){
-                maxAns = max(maxAns, (long long)it.first);
+            if(widths.find(it)!=widths.end()){
+                maxAns = max(maxAns, (long long)it);
             }
         }
         if(maxAns==-1) return -1;
