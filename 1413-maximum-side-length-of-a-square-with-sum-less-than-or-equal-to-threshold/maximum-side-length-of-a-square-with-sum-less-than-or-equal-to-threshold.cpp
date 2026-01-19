b4ad222b17ebc165c714d0224b35e5842271da59
Shreshth Tarwey <151712070+ShreshthTarwey@users.noreply.github.com>
2026-01-20 01:07:53 +0530

    Time: 261 ms (5.36%) | Memory: 32.9 MB (37.05%) - LeetSync

---
+++ 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int findSum(int i, int j, int row, int col, vector<vector<int>>& grid){
+        return grid[row][col] - (i>0 ? grid[i-1][col] : 0) - (j>0 ? grid[row][j-1] : 0) + ((i>0 && j>0) ? grid[i-1][j-1] : 0); 
+    }
+    int maxSideLength(vector<vector<int>>& mat, int threshold) {
+        int n = mat.size();
+        int m = mat[0].size();
+        int ans = 0;
+        vector<vector<int>> prefixSum(n, vector<int>(m, 0));
+        //Building prefix Sum 2D array
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                prefixSum[i][j] = (i>0 ? prefixSum[i-1][j] : 0) +
+                                    (j>0 ? prefixSum[i][j-1] : 0) -
+                                    ((i>0 && j>0) ? prefixSum[i-1][j-1] : 0) + mat[i][j];
+            }
+        }
+
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                //ofset
+                for(int k=0;k<min(n-i,m-j);k++){
+                    int row = i+k;
+                    int col = j+k;
+                    int sum = findSum(i,j,row,col,prefixSum);
+                    if(sum<=threshold){
+                        ans = max(ans,k+1);
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
