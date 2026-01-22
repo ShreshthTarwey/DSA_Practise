082c8d93fe8a261f88e30245b1b44b531be09b5b
Shreshth Tarwey <151712070+ShreshthTarwey@users.noreply.github.com>
2026-01-22 23:28:11 +0530

    Time: 7 ms (72.89%) | Memory: 19.6 MB (22.69%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
-    void removeDup(vector<int>& nums, int index){
-        if(index==nums.size()-1) return;
-        while(index+1<nums.size() && nums[index]==nums[index+1]){
-            nums.erase(nums.begin() + index+1);
-        }
-    }
     int removeDuplicates(vector<int>& nums) {
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i] == nums[i+1]){
-                removeDup(nums, i+1);
+        // if(nums.size()<=2) return nums.size();
+        int i = 0;
+        for(int e=0;e<nums.size();e++){
+            if(i==0 || i==1 || nums[i-2]!=nums[e]){
+                nums[i] = nums[e];
+                i++;
             }
         }
-        return nums.size();
+        return i;
     }
 };
