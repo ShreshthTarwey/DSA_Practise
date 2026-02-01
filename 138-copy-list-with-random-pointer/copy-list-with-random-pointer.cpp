4190b4f8529756a08953881e21f2c03ce213e11c
Shreshth Tarwey <151712070+ShreshthTarwey@users.noreply.github.com>
2026-02-01 11:58:36 +0530

    Time: 5 ms (65.57%) | Memory: 14.9 MB (95.93%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -44,8 +44,48 @@ public:
         }
         return dummy->next;
     }
+    Node* spaceOptimizedMethod(Node* head){
+        Node* temp = head;
+        //Creating interweave list A->A'->B->B'....
+        while(temp){
+            Node* newNode = new Node(temp->val);
+            newNode->next = temp->next;
+            temp->next = newNode;
+            temp = newNode->next;
+        }
+        //Now handeling random pointers
+        Node* oldPointer = head;
+        if(!head) return NULL;
+        Node* newPointer = head->next;
+        while(oldPointer && newPointer){
+            if(!oldPointer->random){
+                newPointer->random = NULL;
+            }
+            else{
+                newPointer->random = oldPointer->random->next;
+            }
+            oldPointer = oldPointer->next->next;
+            if(!newPointer->next) break;
+            newPointer = newPointer->next->next;
+        }
+        //Splliting Lists
+        Node* dummy = new Node(0);
+        Node* tail = dummy;
+        Node* it = head;
+        Node* newList = head->next;
+        Node* it2 = newList;
+        while(it && it2){
+            it->next = it2->next;
+            if(!it2->next) break;
+            it2->next = it2->next->next;
+            it = it->next;
+            it2 = it2->next;
+        }
+        return newList;
+    }
+    
     Node* copyRandomList(Node* head) {
-        return method1(head); //O(n) T.C, O(n) S.C
-        // return spaceOptimizedMethod(head);
+        // return method1(head); //O(n) T.C, O(n) S.C
+        return spaceOptimizedMethod(head);
     }
 };
