d568c7c18fa7b0b1ac02ced4a67dbeaffe765256
Shreshth Tarwey <151712070+ShreshthTarwey@users.noreply.github.com>
2026-02-07 22:40:29 +0530

    Time: 59 ms (14.35%) | Memory: 59.2 MB (7.40%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -40,11 +40,35 @@ public:
         }
         return count;
     }
+    int method3Prefix(string s){
+        vector<int>leftbCount(s.length(), 0);
+        vector<int>rightaCount(s.length(), 0);
+        int bCount = 0;
+        for(int i=0;i<s.length();i++){
+            leftbCount[i] = bCount;
+            if(s[i] == 'b'){
+                bCount++;
+            }
+        }
+        int aCount = 0;
+        for(int i=s.length()-1;i>=0;i--){
+            rightaCount[i] = aCount;
+            if(s[i] == 'a'){
+                aCount++;
+            }
+        }  
+        int minAns = INT_MAX;
+        for(int i=0;i<s.length();i++){
+            minAns = min(minAns, rightaCount[i]+leftbCount[i]);
+        }
+        return minAns;
+    }
     int minimumDeletions(string s) {
         // vector<int>dp(s.length(), -1);
         // return recFun(s, 0, "", dp); //Cannot Apply DP here, and it was giving TLE, in recursion
 
-        return method2Stack(s);
+        // return method2Stack(s); //T.C->O(n), S.C-> O(n)
+        return method3Prefix(s);
 
         
     }
