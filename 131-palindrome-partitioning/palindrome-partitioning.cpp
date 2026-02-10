6e514bb42c35d44e922f773b7506451027bf3cda
Shreshth Tarwey <151712070+ShreshthTarwey@users.noreply.github.com>
2026-02-10 21:49:19 +0530

    Time: 29 ms (91.07%) | Memory: 53.8 MB (69.47%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -10,7 +10,7 @@ public:
         }
         return true;
     }
-    void recFun(string &s, int idx, vector<vector<string>>& ans, vector<string>& temp){
+    void recFun(string &s, int idx, vector<vector<string>>& ans, vector<string>& temp, vector<vector<int>>& dp){
         if(idx>=s.length()){
             if(!temp.empty()){
                 ans.push_back(temp);
@@ -22,18 +22,35 @@ public:
         string tempStr = "";
         for(int i=idx;i<s.length();i++){
             tempStr+=s[i];
-            if(isPalindrome(tempStr)){
+            if(dp[idx][i]){
                 temp.push_back(tempStr);
-                recFun(s, i+1, ans, temp);
+                recFun(s, i+1, ans, temp, dp);
                 temp.pop_back();
             }
         }
 
     }
+    bool preCompute(string &s, vector<vector<int>>& dp, int i, int j){
+        if(i<0 || i>=s.length() || j<0 || j>=s.length()){
+            return false;
+        }
+        if(i>=j) return true;
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        return dp[i][j] = (s[i] == s[j]) && preCompute(s, dp, i+1, j-1);
+    }
     vector<vector<string>> partition(string s) {
+        vector<vector<int>>dpPalin(s.length(), vector<int>(s.length(), -1));
+        int n = s.length();
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                preCompute(s, dpPalin, i, j);
+            }
+        }
+        // preCompute(s, dpPalin, 0, s.length()-1);
         vector<string>temp;
         vector<vector<string>>ans;
-        recFun(s, 0, ans, temp);
+        recFun(s, 0, ans, temp, dpPalin);
         return ans;
     }
 };
