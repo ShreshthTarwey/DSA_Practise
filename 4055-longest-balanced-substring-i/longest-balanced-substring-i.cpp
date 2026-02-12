6526d4448d30642459f39502d3a644af8069ae31
Shreshth Tarwey <151712070+ShreshthTarwey@users.noreply.github.com>
2026-02-12 20:19:56 +0530

    Time: 367 ms (8.13%) | Memory: 27 MB (32.11%) - LeetSync

--- 4055-longest-balanced-substring-i/longest-balanced-substring-i.cpp
+++ 4055-longest-balanced-substring-i/longest-balanced-substring-i.cpp
@@ -19,10 +19,20 @@ public:
         unordered_map<char, int>mp;
         int ans = 0;
         for(int i=0;i<s.length();i++){
+            int distinct = 0;
+            int maxFreq = 0;
             for(int j=i;j<s.length();j++){
                 mp[s[j]]++;
-                if(isBalanced(mp)){
-                    ans = max(ans, j-i+1);
+                // if(isBalanced(mp)){
+                //     ans = max(ans, j-i+1);
+                // } //Optmimzing this thing -------------------------------------------------------
+                int len = j-i+1;
+                if(mp[s[j]] == 1) distinct++;
+                if(maxFreq<mp[s[j]]){
+                    maxFreq = mp[s[j]];
+                }
+                if(len == maxFreq*distinct){
+                    ans = max(ans, len);
                 }
             }
             mp.clear();
