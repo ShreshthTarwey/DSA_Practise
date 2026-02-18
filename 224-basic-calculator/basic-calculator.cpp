9a7d7ea744de7cebbe58aaa0621410b91538206c
Shreshth Tarwey <151712070+ShreshthTarwey@users.noreply.github.com>
2026-02-18 23:15:58 +0530

    Time: 3 ms (58.73%) | Memory: 13.8 MB (31.77%) - LeetSync

--- 224-basic-calculator/basic-calculator.cpp
+++ 224-basic-calculator/basic-calculator.cpp
@@ -35,7 +35,48 @@ public:
         return sum;
     }
     int calculate(string s) {
-        int index = 0;
-        return recFun(s, index);
+        // int index = 0;
+        // return recFun(s, index); ------------------> Recursive Method ---------------------
+
+        // --------------------Stack Method ---------------------------------------------
+
+        stack<long long>st;
+        long long result = 0;
+            int sign  = 1;
+        for(int i=0;i<s.length();i++){
+            char c = s[i];
+            if(c == '('){
+                st.push(result);
+                st.push(sign);
+                result = 0;
+                sign = 1;
+            }
+            else if(c == ')'){
+                if(!st.empty()){
+                    int sign = st.top(); st.pop();
+                    long long prevResult = st.top(); st.pop();
+                    // st.pop();
+                    result = prevResult + (result * sign);
+                    // st.push(prevResult + (result * sign));
+                }
+            }
+            else if(c == '+'){
+                sign = 1;
+            }
+            else if(c == '-'){
+                sign = -1;
+            }
+            else if(isdigit(c)){
+                long long num = 0;
+                while(i<s.length() && isdigit(s[i])){
+                    num = num * 10 + (s[i] - '0');
+                    i++;
+                }
+                i--;
+                result += (sign * num);
+            }
+        }
+        return result;
+
     }
 };
