4f166508ba922ed431825d5f0839eabde2051f12
Shreshth Tarwey <151712070+ShreshthTarwey@users.noreply.github.com>
2026-02-19 20:50:01 +0530

    Time: 12 ms (5.39%) | Memory: 13 MB (61.91%) - LeetSync

--- 696-count-binary-substrings/count-binary-substrings.cpp
+++ 696-count-binary-substrings/count-binary-substrings.cpp
@@ -1,31 +1,57 @@
 class Solution {
 public:
     int countBinarySubstrings(string s) {
-        vector<int>temp;
-        int count = 0;
+        // vector<int>temp;
+        // int count = 0;
+        // for(int i=0;i<s.length();i++){
+        //     if(s[i] == '0'){
+        //         while(i<s.length() && s[i]=='0'){
+        //             count++;
+        //             i++;
+        //         }
+        //         i--;
+        //         temp.push_back(count);
+        //         count = 0;
+        //     }
+        //     else{
+        //         while(i<s.length() && s[i]=='1'){
+        //             count++;
+        //             i++;
+        //         }
+        //         i--;
+        //         temp.push_back(count);
+        //         count = 0;
+        //     }
+        // }
+        // int ans = 0;
+        // for(int i=0;i<temp.size()-1;i++){
+        //     ans += min(temp[i], temp[i+1]);
+        // }
+        // return ans;
+        int prev = 0;
+        int curr = 0;
+        int ans = 0;
         for(int i=0;i<s.length();i++){
             if(s[i] == '0'){
+                prev = curr;
+                curr = 0;
                 while(i<s.length() && s[i]=='0'){
-                    count++;
+                    curr++;
                     i++;
                 }
                 i--;
-                temp.push_back(count);
-                count = 0;
+                ans += min(prev, curr);
             }
             else{
-                while(i<s.length() && s[i]=='1'){
-                    count++;
+                prev = curr;
+                curr = 0;
+                while(i<s.length() && s[i] == '1'){
+                    curr++;
                     i++;
                 }
                 i--;
-                temp.push_back(count);
-                count = 0;
+                ans += min(prev, curr);
             }
-        }
-        int ans = 0;
-        for(int i=0;i<temp.size()-1;i++){
-            ans += min(temp[i], temp[i+1]);
         }
         return ans;
     }
