9d3824a4e521f80bfa290784b4b6db3e8a8693fb
Shreshth Tarwey <151712070+ShreshthTarwey@users.noreply.github.com>
2026-02-20 20:36:16 +0530

    Time: 2 ms (23.31%) | Memory: 9.2 MB (80.20%) - LeetSync

--- 394-decode-string/decode-string.cpp
+++ 394-decode-string/decode-string.cpp
@@ -24,8 +24,50 @@ public:
         }
         return res;
     }
+    string stackMethod(string &s){
+        stack<char>st;
+        for(int i=0;i<s.length();i++){
+            if(s[i] != ']'){
+                st.push(s[i]);
+                continue;
+            }
+            string str = "";
+            while(!st.empty() && st.top()!='['){
+                str+=st.top();
+                st.pop();
+            }
+            reverse(str.begin(), str.end());
+            st.pop(); //Popping extra [ bracket
+            //Fetching number
+            int num = 0;
+            string numstr = "";
+            while(!st.empty() && isdigit((unsigned char)st.top())){
+                numstr+=st.top();
+                st.pop();
+            }
+            reverse(numstr.begin(), numstr.end());
+            if(numstr!=""){
+                num = stoi(numstr);
+                string expanded = "";
+                for(int i=0;i<num;i++){
+                    expanded+=str;
+                }
+                for(char c : expanded)
+                    st.push(c);
+            }
+        }
+        string res = "";
+        while(!st.empty()){
+            res+=st.top();
+            st.pop();
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
     string decodeString(string s) {
-        int index = 0;
-        return recFun(s,index);
+        // int index = 0;
+        // return recFun(s,index);// Done with recursion
+
+        return stackMethod(s);
     }
 };
