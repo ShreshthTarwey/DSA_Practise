35424dc0f6df19610c654e91676e1fc60e4fe41c
Shreshth Tarwey <151712070+ShreshthTarwey@users.noreply.github.com>
2026-02-25 17:39:40 +0530

    Time: 15 ms (29.64%) | Memory: 24 MB (5.10%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.cpp
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -58,8 +58,8 @@ public:
         return maxArea;
 
     }
-    int maximalRectangle(vector<vector<char>>& matrix) {
-        int n = matrix.size();
+    int solution(vector<vector<char>>& matrix){
+                int n = matrix.size();
         int m = matrix[0].size();
         vector<int>height(m);
         for(int i=0;i<m;i++){
@@ -80,6 +80,65 @@ public:
             maxArea = max(maxArea, findMaxArea(height));
         }
         return maxArea;
+    }
 
+    int largestAreaHistogram(vector<int>& heights){
+        stack<int>st1, st2;
+        int n = heights.size();
+        vector<int>prevSmaller(n, -1);
+        vector<int>nextSmaller(n, n);
+
+        //Filling prevSmaller
+        for(int i=0;i<n;i++){
+            while(!st1.empty() && heights[st1.top()]>=heights[i]){
+                st1.pop();
+            }
+            if(!st1.empty()){
+                prevSmaller[i] = st1.top();
+            }
+            st1.push(i);
+        }
+
+        //Filling nextSmaller
+
+        for(int i=n-1;i>=0;i--){
+            while(!st2.empty() && heights[st2.top()]>=heights[i]){
+                st2.pop();
+            }
+            if(!st2.empty()){
+                nextSmaller[i] = st2.top();
+            }
+            st2.push(i);
+        }
+        int ans = INT_MIN;
+        for(int i=0;i<n;i++){
+            int leftWidth = i - prevSmaller[i] - 1;
+            int rightWidth = nextSmaller[i] - i - 1;
+            int area = (leftWidth + 1 + rightWidth) * (heights[i]);
+            ans = max(ans, area);
+        }
+        return ans;
+    }
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        // return solution(matrix);
+        //---------------------------------------------Revision-----------------------------------------------
+
+        vector<vector<int>>v(matrix.size(), vector<int>(matrix[0].size()));
+
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<matrix[0].size();j++){
+                v[i][j] = (matrix[i][j] - '0');
+            }
+        }
+
+        int area = largestAreaHistogram(v[0]);
+        for(int i=1;i<matrix.size();i++){
+            for(int j=0;j<matrix[0].size();j++){
+                if(v[i][j] != 0)
+                    v[i][j] += v[i-1][j];
+            }
+            area = max(area, largestAreaHistogram(v[i]));
+        }
+        return area;
     }
 };
