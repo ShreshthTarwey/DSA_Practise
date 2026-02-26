536fd26a99eaae32446bc74de4f03fae43fe49fe
Shreshth Tarwey <151712070+ShreshthTarwey@users.noreply.github.com>
2026-02-26 11:06:45 +0530

    Time: 18 ms (23.69%) | Memory: 24 MB (5.10%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.cpp
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -119,10 +119,7 @@ public:
         }
         return ans;
     }
-    int maximalRectangle(vector<vector<char>>& matrix) {
-        // return solution(matrix);
-        //---------------------------------------------Revision-----------------------------------------------
-
+    int revision(vector<vector<char>>& matrix){
         vector<vector<int>>v(matrix.size(), vector<int>(matrix[0].size()));
 
         for(int i=0;i<matrix.size();i++){
@@ -138,6 +135,67 @@ public:
                     v[i][j] += v[i-1][j];
             }
             area = max(area, largestAreaHistogram(v[i]));
+        }
+        return area;
+    }
+    int fun(vector<int>& arr){
+        stack<int>st1,st2;
+        int n = arr.size();
+        vector<int>prevSmaller(n, -1);
+        vector<int>nextSmaller(n, n);
+
+        //Filling prevSamller
+        for(int i=0;i<n;i++){
+            while(!st1.empty() && arr[st1.top()]>=arr[i]){
+                st1.pop();
+            }
+            if(!st1.empty()){
+                prevSmaller[i] = st1.top();
+            }
+            st1.push(i);
+        }
+
+        //Filling nextSmaller
+        for(int i=n-1;i>=0;i--){
+            while(!st2.empty()  && arr[st2.top()]>=arr[i]){
+                st2.pop();
+            }
+            if(!st2.empty()){
+                nextSmaller[i] = st2.top();
+            }
+            st2.push(i);
+        }
+
+        int ans = INT_MIN;
+        for(int i=0;i<n;i++){
+            int leftWidth = i - prevSmaller[i] - 1;
+            int rightWidth = nextSmaller[i] - i - 1;
+            ans = max(ans, (leftWidth + rightWidth + 1)*arr[i]);
+        }
+        return ans;
+    }
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        // return solution(matrix);
+        //---------------------------------------------Revision-----------------------------------------------
+        // return revision(matrix);
+        // --------------------------------------------Revison2-----------------------------------------------
+
+        vector<vector<int>>v(matrix.size(), vector<int>(matrix[0].size()));
+
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<matrix[0].size();j++){
+                v[i][j] = (matrix[i][j] - '0');
+            }
+        }
+
+        int area = fun(v[0]);
+        for(int i=1;i<v.size();i++){
+            for(int j=0;j<v[0].size();j++){
+                if(v[i][j]!=0){
+                    v[i][j] += v[i-1][j];
+                }
+            }
+            area = max(area, fun(v[i]));
         }
         return area;
     }
