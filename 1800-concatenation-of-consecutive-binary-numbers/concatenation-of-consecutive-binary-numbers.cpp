066a0dc05285263965804dc42e9e8e4dc36d8719
Shreshth Tarwey <151712070+ShreshthTarwey@users.noreply.github.com>
2026-02-28 19:04:28 +0530

    Time: 44 ms (59.06%) | Memory: 8.1 MB (69.80%) - LeetSync

--- 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.cpp
+++ 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.cpp
@@ -1,12 +1,27 @@
 class Solution {
 public:
     const int MOD = 1e9+7;
-    int concatenatedBinary(int n) {
+    int method1(int n){
         long long res = 0;
         for(int i=1;i<=n;i++){
-            int digits = log2(i) + 1;
+            int digits = log2(i) + 1; //Remember it, how much digit or bits it will take to represent any number
             res = ((res<<digits)%MOD + i)%MOD;
         }
         return res;
+    }
+    int method2(int n){
+        long long res = 0;
+        int bits = 0;
+        for(int i=1;i<=n;i++){
+            if((i&(i-1))==0){
+                bits++;
+            }
+            res = ((res<<bits)%MOD + i) % MOD;
+        }
+        return res;
+    }
+    int concatenatedBinary(int n) {
+        // return method1(n); //This is for the one who remembers how to calculate how much bit a number will take to represent
+        return method2(n);
     }
 };
