0fca0452e718b09b07b408f3174731a444719292
Shreshth Tarwey <151712070+ShreshthTarwey@users.noreply.github.com>
2026-03-07 22:39:17 +0530

    Time: 7 ms (91.60%) | Memory: 14.4 MB (84.80%) - LeetSync

--- 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
+++ 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
@@ -80,10 +80,44 @@ public:
         }
         return ans <= 0 ? 0 : ans;
     }
+    int method3(string &s){
+        int n = s.length();
+        int flip1 = 0;
+        int flip2 = 0;
+        int i = 0;
+        int j = 0;
+        int ans = INT_MAX;
+        while(j<2*n){
+            char expectedChar1 = (j%2==0 ? '0' : '1');
+            char expectedChar2 = (j%2==0 ? '1' : '0');
+            if(s[j%n] != expectedChar1){
+                flip1++;
+            }
+            if(s[j%n] != expectedChar2){
+                flip2++;
+            }
+            if(j-i+1>n){
+                char c1 = (i%2==0 ? '0' : '1');
+                char c2 = (i%2==0 ? '1' : '0'); 
+                if(s[i%n] != c1){
+                    flip1--;
+                }
+                if(s[i%n] != c2){
+                    flip2--;
+                }
+                i++;
+            }
+            if(j-i+1 == n){
+                ans = min(ans, min(flip1, flip2));
+            }
+            j++;
+        }
+        return ans;
+    }
     int minFlips(string s) { 
         // return method1(s); //T.C-> O(n), S.C->O(2*n)
-        return method2(s); //T.C-> O(n), S.C->O(n)
-
+        // return method2(s); //T.C-> O(n), S.C->O(n)
+        return method3(s); //T.C-> O(n), S.C->O(1)
         
     }
 };
