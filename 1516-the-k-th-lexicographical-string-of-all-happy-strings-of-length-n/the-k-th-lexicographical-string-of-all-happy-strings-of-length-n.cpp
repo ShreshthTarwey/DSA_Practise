f4bb57d6347864a2aa572ef8df46d6d446f3cb82
Shreshth Tarwey <151712070+ShreshthTarwey@users.noreply.github.com>
2026-03-14 19:09:56 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (80.34%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,29 +1,37 @@
 class Solution {
 public:
-    void recFun(string &str, int n, int k, vector<char>& list, int &counter, vector<string>& arr){
-        if(str.length()==n){
+    void recFun(string& str, int n, int k, vector<char>& list, int& counter,
+                string& res) {
+        if (str.length() == n) {
             counter++;
-            arr.push_back(str);
+            // arr.push_back(str);
+            if (counter == k) {
+                res = str;
+                // return;
+            }
             return;
         }
-        if(counter == k) return;
-        for(int i=0;i<3;i++){
-            if(!str.empty() && list[i] == str.back()){
+
+        for (int i = 0; i < 3; i++) {
+            if (!str.empty() && list[i] == str.back()) {
                 continue;
             }
+            if (counter == k)
+                return;
             str.push_back(list[i]);
-            recFun(str, n, k, list, counter, arr);
+            recFun(str, n, k, list, counter, res);
             str.pop_back();
         }
     }
     string getHappyString(int n, int k) {
         string str = "";
         string res = "";
-        vector<char>list = {'a', 'b', 'c'};
+        vector<char> list = {'a', 'b', 'c'};
         int counter = 0;
-        vector<string>arr;
-        recFun(str, n, k, list, counter, arr);
-        if(arr.size()<k) return "";
-        return arr[k-1];
+        // vector<string>arr;
+        recFun(str, n, k, list, counter, res);
+        // if(arr.size()<k) return "";
+        // return arr[k-1];
+        return res;
     }
 };
