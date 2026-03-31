befc01eda7b163c844f35377ebc40753591c2f37
Shreshth Tarwey <151712070+ShreshthTarwey@users.noreply.github.com>
2026-04-01 00:22:28 +0530

    Time: 44 ms (78.28%) | Memory: 54.5 MB (9.77%) - LeetSync

---
+++ 2762-cache-with-time-limit/cache-with-time-limit.js
@@ -0,0 +1,49 @@
+var TimeLimitedCache = function() {
+    this.cache = new Map();
+};
+
+/** 
+ * @param {number} key
+ * @param {number} value
+ * @param {number} duration time until expiration in ms
+ * @return {boolean} if un-expired key already existed
+ */
+TimeLimitedCache.prototype.set = function(key, value, duration) {
+    let timerID = setTimeout(()=>{
+            this.cache.delete(key);
+        }, duration);
+    if(this.cache.has(key)){
+        clearTimeout(this.cache.get(key).timerID);
+        this.cache.set(key, {value, timerID});
+        return true;
+    }
+    else{
+        this.cache.set(key, {value, timerID});
+        return false;
+    }
+};
+
+/** 
+ * @param {number} key
+ * @return {number} value associated with key
+ */
+TimeLimitedCache.prototype.get = function(key) {
+    if(this.cache.has(key)){
+        return this.cache.get(key).value;
+    }
+    return -1;
+};
+
+/** 
+ * @return {number} count of non-expired keys
+ */
+TimeLimitedCache.prototype.count = function() {
+    return this.cache.size;
+};
+
+/**
+ * const timeLimitedCache = new TimeLimitedCache()
+ * timeLimitedCache.set(1, 42, 1000); // false
+ * timeLimitedCache.get(1) // 42
+ * timeLimitedCache.count() // 1
+ */
