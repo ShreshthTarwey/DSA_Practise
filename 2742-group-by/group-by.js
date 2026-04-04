b80b49a59e1296a4ffa55546b0e66b6fe3e063a2
Shreshth Tarwey <151712070+ShreshthTarwey@users.noreply.github.com>
2026-04-04 21:54:45 +0530

    Time: 100 ms (77.42%) | Memory: 80.6 MB (10.90%) - LeetSync

---
+++ 2742-group-by/group-by.js
@@ -0,0 +1,19 @@
+/**
+ * @param {Function} fn
+ * @return {Object}
+ */
+Array.prototype.groupBy = function(fn) {
+    let res = {};
+    for(let item of this){
+        let key = fn(item);
+        if(!(key in res)){
+            res[key] = [];
+        }
+        res[key].push(item);
+    }
+    return res;
+};
+
+/**
+ * [1,2,3].groupBy(String) // {"1":[1],"2":[2],"3":[3]}
+ */
