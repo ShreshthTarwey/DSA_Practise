93aa88c531021500ccf80c4a3656600c1308c932
Shreshth Tarwey <151712070+ShreshthTarwey@users.noreply.github.com>
2026-04-05 18:43:27 +0530

    Time: 3 ms (76.36%) | Memory: 19.3 MB (54.84%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool dfs(int node, vector<vector<int>>& adjList, vector<bool>& visited, vector<bool>& pathVisited){
+        visited[node] = true;
+        pathVisited[node] = true;
+        for(auto n: adjList[node]){
+            if(!visited[n]){
+                if(dfs(n, adjList, visited, pathVisited)){
+                    return true;
+                }
+            }
+            else if(pathVisited[n]){
+                return true;
+            }
+        }
+        pathVisited[node] = false;
+        return false;
+    }
+    bool method1(int numCourses, vector<vector<int>>& prerequisites){
+        vector<bool>visited(numCourses, false);
+        vector<bool>pathVisited(numCourses, false);
+        vector<vector<int>>adjList(numCourses);
+        for(auto &it: prerequisites){
+            adjList[it[0]].push_back(it[1]);
+        }
+        for(int i=0;i<numCourses;i++){
+            if(dfs(i, adjList, visited, pathVisited)){
+                return false;
+            }
+        }
+        return true;
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        return method1(numCourses, prerequisites);
+    }
+};
