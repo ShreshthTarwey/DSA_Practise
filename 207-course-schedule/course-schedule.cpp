1d56964bb7529b4d63ee9f1ab57e7cd4525b1d46
Shreshth Tarwey <151712070+ShreshthTarwey@users.noreply.github.com>
2026-04-05 19:07:50 +0530

    Time: 4 ms (64.38%) | Memory: 19.3 MB (54.84%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -31,8 +31,40 @@ public:
         }
         return true;
     }
+    bool method2(int numCourses, vector<vector<int>>& prerequisites){
+        vector<int>indegree(numCourses, 0);
+        vector<vector<int>>adjList(numCourses);
+        for(auto &it: prerequisites){
+            adjList[it[0]].push_back(it[1]);
+        }
+        //Filling indegree
+        for(auto &it : adjList){
+            for(auto &jt: it){
+                indegree[jt]++;
+            }
+        }
+        queue<int>q;
+        for(int i=0;i<indegree.size();i++){
+            if(!indegree[i]){
+                q.push(i);
+            }
+        }
+        vector<int>temp;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            temp.push_back(node);
+            for(auto it: adjList[node]){
+                indegree[it]--;
+                if(!indegree[it]){
+                    q.push(it);
+                }
+            }
+        }
+        return temp.size() == numCourses;
+    }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        return method1(numCourses, prerequisites); //DFS on Directed Graph
-
+        // return method1(numCourses, prerequisites); //DFS on Directed Graph
+        return method2(numCourses, prerequisites); //BFS on Directed Graph, Also known as khan's algorithm
     }
 };
