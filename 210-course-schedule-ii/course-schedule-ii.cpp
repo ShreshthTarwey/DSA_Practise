0dde258a3e8029b638f87ec9e719c08854eb72f3
Shreshth Tarwey <151712070+ShreshthTarwey@users.noreply.github.com>
2026-04-05 21:15:15 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (52.03%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -31,7 +31,48 @@ public:
         }
         return temp.size() == numCourses ? temp : vector<int>();
     }
+    bool dfs(vector<vector<int>>& adjList, int node, vector<bool>& visited, stack<int>& st, vector<bool>& pathVisited){
+        
+        visited[node] = true;
+        pathVisited[node] = true;
+        for(auto &it: adjList[node]){
+            if(!visited[it]){
+                if(dfs(adjList, it, visited, st, pathVisited)){
+                    return true;
+                }
+            }
+            else if(pathVisited[it]){
+                return true;
+            }
+        }
+        st.push(node);
+        pathVisited[node] = false;
+        return false;
+    }
+    vector<int> usingDFS(int numCourses, vector<vector<int>>& prerequisites){
+        vector<vector<int>>adjList(numCourses);
+        for(auto &it: prerequisites){
+            adjList[it[1]].push_back(it[0]);
+        }
+        vector<int>res;
+        stack<int>st;
+        vector<bool>visited(numCourses, false);
+        vector<bool>pathVisited(numCourses, false);
+        for(int i=0;i<numCourses;i++){
+            if(!visited[i]){
+                if(dfs(adjList, i, visited, st, pathVisited)){
+                    return vector<int>();
+                }
+            }
+        }
+        while(!st.empty()){
+            res.push_back(st.top());
+            st.pop();
+        }
+        return res.size() == numCourses ? res : vector<int>();
+    }
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        return khansAlgo(numCourses, prerequisites);
+        // return khansAlgo(numCourses, prerequisites);
+        return usingDFS(numCourses, prerequisites);
     }
 };
