9a86e5a78fa973cc6077d7cf984c3e34d1692b0b
Shreshth Tarwey <151712070+ShreshthTarwey@users.noreply.github.com>
2026-04-21 23:57:54 +0530

    Time: 132 ms (84.29%) | Memory: 153.9 MB (47.12%) - LeetSync

--- 1840-minimize-hamming-distance-after-swap-operations/minimize-hamming-distance-after-swap-operations.cpp
+++ 1840-minimize-hamming-distance-after-swap-operations/minimize-hamming-distance-after-swap-operations.cpp
@@ -1,4 +1,37 @@
-
+class DSU{
+    vector<int>parent;
+    vector<int>rank;
+    public:
+    DSU(int n){
+        parent.resize(n);
+        rank.resize(n, 0);
+        for(int i=0;i<n;i++){
+            parent[i] = i;
+        }
+    }
+    int find(int x){
+        if(parent[x] != x){
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+    void unionByRank(int x, int y){
+        int p1 = find(x);
+        int p2 = find(y);
+        if(p1 != p2){
+            if(rank[p1]>rank[p2]){
+                parent[p2] = p1;
+            }
+            else if(rank[p1]<rank[p2]){
+                parent[p1] = p2;
+            }
+            else{
+                parent[p2] = p1;
+                rank[p1]++;
+            }
+        }
+    }
+};
 class Solution {
 public:
     void dfs(vector<vector<int>>& adjList, vector<bool>& visited, vector<int>& components, int node){
@@ -10,7 +43,7 @@ public:
             }
         }
     }
-    int minimumHammingDistance(vector<int>& source, vector<int>& target, vector<vector<int>>& allowedSwaps) {
+    int dfsMethod(vector<int>& source, vector<int>& target, vector<vector<int>>& allowedSwaps){
         int n = source.size();
         int mismatch = 0;
         vector<vector<int>>adjList(n);
@@ -41,5 +74,31 @@ public:
             }
         }
         return mismatch;
+    }
+    int dsuMethod(vector<int>& source, vector<int>& target, vector<vector<int>>& allowedSwaps){
+        int n = source.size();
+        int mismatch = 0;
+        DSU dsu(n);
+        unordered_map<int, unordered_map<int, int>>mp;
+        for(auto &it: allowedSwaps){
+            dsu.unionByRank(it[0], it[1]);
+        }
+        for(int i=0;i<n;i++){
+            mp[dsu.find(i)][source[i]]++;
+        }
+        for(int i=0;i<n;i++){
+            int parent = dsu.find(i);
+            if(mp[parent][target[i]]>=1){
+                mp[parent][target[i]]--;
+            }
+            else{
+                mismatch++;
+            }
+        }
+        return mismatch;
+    }
+    int minimumHammingDistance(vector<int>& source, vector<int>& target, vector<vector<int>>& allowedSwaps) {
+        // return dfsMethod(source, target, allowedSwaps);
+        return dsuMethod(source, target, allowedSwaps);
     }
 };
