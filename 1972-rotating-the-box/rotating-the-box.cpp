de65952a0136356bd225d2b1c9d5ba9545acc9d5
Shreshth Tarwey <151712070+ShreshthTarwey@users.noreply.github.com>
2026-05-06 21:31:02 +0530

    Time: 9 ms (73.22%) | Memory: 58.6 MB (12.53%) - LeetSync

--- 1972-rotating-the-box/rotating-the-box.cpp
+++ 1972-rotating-the-box/rotating-the-box.cpp
@@ -25,24 +25,36 @@ public:
         int n = boxGrid.size();
         int m = boxGrid[0].size();
         for(int i=0;i<n;i++){
-            int space = findSpace(boxGrid[i], m-1);
-            int j = space-1;
-            if(space<0 || j<0){
-                continue;
-            }
-            while(j>=0){
-                if(boxGrid[i][j] == '#'){
+            int space = m-1;
+            for(int j=m-1;j>=0;j--){
+                if(boxGrid[i][j] == '*'){
+                    space = j-1;
+                }
+                else if(boxGrid[i][j] == '#'){
                     swap(boxGrid[i][j], boxGrid[i][space]);
-                    space = findSpace(boxGrid[i], space-1);
-                    // space = j;
+                    space--;
                 }
-                else if(boxGrid[i][j] == '*'){
-                    space = findSpace(boxGrid[i], j-1);
-                    j = space;
-                }
-                j--;
             }
         }
+        // for(int i=0;i<n;i++){
+        //     int space = findSpace(boxGrid[i], m-1);
+        //     int j = space-1;
+        //     if(space<0 || j<0){
+        //         continue;
+        //     }
+        //     while(j>=0){
+        //         if(boxGrid[i][j] == '#'){
+        //             swap(boxGrid[i][j], boxGrid[i][space]);
+        //             space = findSpace(boxGrid[i], space-1);
+        //             // space = j;
+        //         }
+        //         else if(boxGrid[i][j] == '*'){
+        //             space = findSpace(boxGrid[i], j-1);
+        //             j = space;
+        //         }
+        //         j--;
+        //     }
+        // }
         boxGrid = rotate(boxGrid);
         return boxGrid;
     }
