5a4892b6f03c5fbae059d8feddd997c8adb78d88
Shreshth Tarwey <151712070+ShreshthTarwey@users.noreply.github.com>
2026-06-21 16:32:02 +0530

    Time: 51 ms (80.82%) | Memory: 106.6 MB (84.93%) - LeetSync

--- 1968-maximum-building-height/maximum-building-height.cpp
+++ 1968-maximum-building-height/maximum-building-height.cpp
@@ -1,21 +1,31 @@
 class Solution {
 public:
     int maxBuilding(int n, vector<vector<int>>& restrictions) {
-        auto&& r = restrictions;
-        r.push_back({1, 0});
-        sort(r.begin(), r.end());
-        if (r[r.size() - 1][0] != n) r.push_back({n, n - 1});
-        int m = r.size();
-        for (int i = 1; i < m; ++i) {
-            r[i][1] = min(r[i][1], r[i - 1][1] + r[i][0] - r[i - 1][0]);
+        restrictions.push_back({1, 0});
+        restrictions.push_back({n, n-1});
+        sort(restrictions.begin(), restrictions.end());
+        int size = restrictions.size();
+        //Left to right correction
+        for(int i=1;i<size;i++){
+            int dist = restrictions[i][0] - restrictions[i-1][0];
+            restrictions[i][1] = min(restrictions[i][1], dist+restrictions[i-1][1]);
         }
-        for (int i = m - 2; i > 0; --i) {
-            r[i][1] = min(r[i][1], r[i + 1][1] + r[i + 1][0] - r[i][0]);
+        //Right to left;
+        for(int i=size-2;i>=0;i--){
+            int dist = restrictions[i+1][0] - restrictions[i][0]; 
+            restrictions[i][1] = min(restrictions[i][1], dist+restrictions[i+1][1]);
         }
+        //Finding the answer
         int ans = 0;
-        for (int i = 0; i < m - 1; ++i) {
-            int t = (r[i][1] + r[i + 1][1] + r[i + 1][0] - r[i][0]) / 2;
-            ans = max(ans, t);
+        for(int i=1;i<size;i++){
+            int leftPos = restrictions[i-1][0];
+            int leftHeight = restrictions[i-1][1];
+
+            int rightPos = restrictions[i][0];
+            int rightHeight = restrictions[i][1];
+            int dist = rightPos - leftPos;
+            int diff = abs(leftHeight - rightHeight);
+            ans = max(ans, max(leftHeight, rightHeight) + (dist - diff)/2);
         }
         return ans;
     }
