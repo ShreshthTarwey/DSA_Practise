2e319a7da0abe6484c563a324164230c1670bcee
Shreshth Tarwey <151712070+ShreshthTarwey@users.noreply.github.com>
2026-07-15 16:30:53 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (9.11%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        int sumOdd = 0;
-        int sumEven = 0;
-        int even = 2;
-        int odd = 1;
-        for(int i=0;i<n;i++){
-            even+=2;
-            odd+=2;
-            sumOdd+=odd;
-            sumEven+=even;
-        }
-        // cout<<sumOdd<<" "<<sumEven<<endl;
-        return gcd(sumOdd, sumEven);
+        // int sumOdd = 0;
+        // int sumEven = 0;
+        // int even = 2;
+        // int odd = 1;
+        // for(int i=0;i<n;i++){
+        //     even+=2;
+        //     odd+=2;
+        //     sumOdd+=odd;
+        //     sumEven+=even;
+        // }
+        // // cout<<sumOdd<<" "<<sumEven<<endl;
+        int sumE = n*n;
+        int evenS = n*(n+1);
+        return gcd(sumE, evenS);
 
     }
 };
