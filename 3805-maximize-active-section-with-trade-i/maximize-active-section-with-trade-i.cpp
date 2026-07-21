4491354b82830af4439c8eeb8917ee2d179e2d24
Shreshth Tarwey <151712070+ShreshthTarwey@users.noreply.github.com>
2026-07-21 22:47:01 +0530

    Time: 82 ms (42.79%) | Memory: 104.8 MB (29.73%) - LeetSync

--- 3805-maximize-active-section-with-trade-i/maximize-active-section-with-trade-i.cpp
+++ 3805-maximize-active-section-with-trade-i/maximize-active-section-with-trade-i.cpp
@@ -4,6 +4,7 @@ public:
         vector<int>arr;
         int n = s.length();
         int i=0;
+        int cnt1 = 0;
         while(i<n){
             int count1 = 0;
             while(i<n && s[i]-'0'>0){
@@ -12,6 +13,7 @@ public:
             }
             if(count1>0)
                 arr.push_back(count1);
+            cnt1 += count1;
             int count2 = 0;
             while(i<n && s[i]-'0' == 0){
                 count2++;
@@ -24,12 +26,11 @@ public:
         int m = arr.size();
         int ans = INT_MIN;
         i=0;
-        int cnt1 = 0;
-        for(int i=0;i<m;i++){
-            if(arr[i]>0){
-                cnt1 += arr[i];
-            }
-        }
+        // for(int i=0;i<m;i++){
+        //     if(arr[i]>0){
+        //         cnt1 += arr[i];
+        //     }
+        // }
         ans = cnt1;
         int j=2;
         while(j<m){
