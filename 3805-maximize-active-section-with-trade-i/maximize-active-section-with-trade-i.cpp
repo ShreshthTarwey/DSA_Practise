1157c31e6d91c94fc422b63d40f90111ab9773c0
Shreshth Tarwey <151712070+ShreshthTarwey@users.noreply.github.com>
2026-07-21 22:42:07 +0530

    Time: 78 ms (47.30%) | Memory: 105 MB (28.38%) - LeetSync

---
+++ 3805-maximize-active-section-with-trade-i/maximize-active-section-with-trade-i.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int maxActiveSectionsAfterTrade(string s) {
+        vector<int>arr;
+        int n = s.length();
+        int i=0;
+        while(i<n){
+            int count1 = 0;
+            while(i<n && s[i]-'0'>0){
+                count1++;
+                i++;
+            }
+            if(count1>0)
+                arr.push_back(count1);
+            int count2 = 0;
+            while(i<n && s[i]-'0' == 0){
+                count2++;
+                i++;
+            }
+            if(count2>0)
+                arr.push_back(-count2);
+            
+        }
+        int m = arr.size();
+        int ans = INT_MIN;
+        i=0;
+        int cnt1 = 0;
+        for(int i=0;i<m;i++){
+            if(arr[i]>0){
+                cnt1 += arr[i];
+            }
+        }
+        ans = cnt1;
+        int j=2;
+        while(j<m){
+            if(arr[i]<0 || arr[j]<0){
+                ans = max(ans, cnt1 + abs(arr[i]) + abs(arr[j]));
+            }
+            i++;
+            j++;
+        }
+        return ans;
+    }
+};
