9a48ca47e991886ffd3d63738c2cde2585a1cdda
Shreshth Tarwey <151712070+ShreshthTarwey@users.noreply.github.com>
2026-07-21 15:52:34 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (35.09%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
-    int findPivot(vector<int>& nums, int l, int r){
+    int binarySearch1(vector<int>& nums){
+        int l = 0;
+        int r = nums.size()-1;
         while(l<=r){
-            int mid = l + (r-l)/2;
-            if(mid-1>=0 && nums[mid-1] > nums[mid]){
+            int mid = (l+r)/2;
+            if(mid-1>=0 && nums[mid-1]>nums[mid]){
                 return mid;
             }
             else if(nums[mid]<nums[r]){
@@ -13,30 +15,30 @@ public:
                 l = mid+1;
             }
         }
-        return 0;
+        return -1;
     }
-    int binarySearch(vector<int>& nums, int l, int r, int target){
-        while(l<=r){
-            int mid = l + (r-l)/2;
+    int binarySearch2(vector<int>& nums, int start, int end, int target){
+        while(start<=end){
+            int mid = (start+end)/2;
             if(nums[mid] == target){
                 return mid;
             }
             else if(nums[mid]>target){
-                r = mid-1;
+                end = mid-1;
             }
             else{
-                l = mid+1;
+                start = mid+1;
             }
         }
         return -1;
     }
     int search(vector<int>& nums, int target) {
-        int pivot = findPivot(nums, 0, nums.size()-1);
-        if(target>=nums[pivot] && target<=nums[nums.size()-1]){
-            return binarySearch(nums, pivot, nums.size()-1, target);
-        }
-        else{
-            return binarySearch(nums, 0, pivot-1, target);
-        }
+        int pivot = binarySearch1(nums);
+        if(pivot == -1){
+    return binarySearch2(nums,0,nums.size()-1,target);
+}
+        int opt1 = binarySearch2(nums, pivot, nums.size()-1, target);
+        int opt2 = binarySearch2(nums, 0, pivot-1, target);
+        return opt1 == -1 ? opt2: opt1;
     }
 };
