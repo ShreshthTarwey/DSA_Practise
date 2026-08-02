20b0eb54418f739b0ddb0d4caf985b07c92884a9
Shreshth Tarwey <151712070+ShreshthTarwey@users.noreply.github.com>
2026-08-02 11:44:46 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (98.71%) - LeetSync

---
+++ 909-stone-game/stone-game.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    // int recFun(vector<int>& piles, int i, int j){
+    //     if(i>j){
+    //         return 0;
+    //     }
+    //     int take_i = piles[i] + min(recFun())
+    // }
+    bool stoneGame(vector<int>& piles) {
+        // int p1 = recFun(piles, 0, piles.size()-1);
+        // int sum = accumulate(piles.begin(), piles.end(), 0);
+        // int p2 = sum - p1;
+        // return p1>=p2;
+        return true;
+    }
+};
