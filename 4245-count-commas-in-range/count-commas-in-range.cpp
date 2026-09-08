568ef28c49a24825d133b3915c90bd3a98a95765
Shreshth Tarwey <151712070+ShreshthTarwey@users.noreply.github.com>
2026-09-08 22:42:19 +0530

    Time: 621 ms (5.13%) | Memory: 9.2 MB (5.52%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.cpp
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int countCommas(int n) {
         int commas = 0;
-        for(int i=1;i<=n;i++){
+        for(int i=1000;i<=n;i++){
             string num = to_string(i);
             int len = num.length();
             commas += (len-1)/3;
